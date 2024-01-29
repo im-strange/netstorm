d2a4d87c098d8c6c9b6cf110ecbbb16320e41bfd
im-strange <itsmestrange.dump@gmail.com>
2024-01-29 20:35:38 +0800

    update

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup
 
 setup(
-  name="netstorm",
-  version="1.0.0",
-  url="https://github.com/im-strange/netstorm.git",
-  author="Samuel Genoguin",
-  description="DDos with Python and Socket",
-  include_package_data=True,
-  packages=[],
-  package_data={}
+      name="netstorm",
+      version="1.0.0",
+      url="https://github.com/im-strange/netstorm.git",
+      author="Samuel Genoguin",
+      description="DDos with Python and Socket",
+      py_modules=["netstorm"],
+      install_requires=[
+          "socket",
+          "threading",
+          "random",
+          "sys"
+      ],
+      include_package_data=True,
+      packages=[],
+      package_data={},
+      entry_points={
+          "console_scripts": ["netstorm = netstorm:main"]
+      }
 )
